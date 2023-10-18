66953a5136366c6cbee9fab65fbd6484bd91333a
Siam-Nurnabi <siamnurnabi@gmail.com>
2023-10-18 20:37:57 +0600

    PatientHistoryDto used is commented

--- src/main/java/com/example/Baymax/controller/bay/PatientHistoryController.java
+++ src/main/java/com/example/Baymax/controller/bay/PatientHistoryController.java
@@ -1,15 +1,15 @@
 package com.example.Baymax.controller.bay;
 
-import com.example.Baymax.dto.bay.PatientHistoryDto;
 import com.example.Baymax.model.bay.PatientHistory;
 import com.example.Baymax.service.bay.PatientHistoryService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
-
-import javax.validation.Valid;
 
 @Controller
 @RequestMapping(path = "history")
@@ -28,15 +28,16 @@ public class PatientHistoryController {
     }
 
     @GetMapping("/create")
-    public String create(Model model) {
-        PatientHistoryDto patientHistoryDto = new PatientHistoryDto();
-        model.addAttribute("patientHistoryDto", patientHistoryDto);
+    public String upload(Model model) {
+//        PatientHistoryDto patientHistoryDto = new PatientHistoryDto();
+//        model.addAttribute("patientHistoryDto", patientHistoryDto);
         return "/PatientHistory/Upload";
     }
 
     @PostMapping("/upload")
-    public String create(@Valid @ModelAttribute("patientDto") PatientHistoryDto patientHistoryDto,
-                         @RequestParam("image") MultipartFile file, BindingResult result, Model model) {
+    public String upload(@RequestParam("image") MultipartFile file, BindingResult result, Model model
+//                         @Valid @ModelAttribute("patientDto") PatientHistoryDto patientHistoryDto,
+    ) {
 //        List<String> errors = new ArrayList<>();
 //        Patient patient = patientHistoryService.toPatient(patientDto);
         PatientHistory patientHistory = new PatientHistory();
